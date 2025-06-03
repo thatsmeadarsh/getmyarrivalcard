cb3f5573bd31df9815128c284a69d78edfb130ed
thatsmeadarsh <32561750+thatsmeadarsh@users.noreply.github.com>
2025-06-03 21:33:59 +0530

    Add built client files with PDF extraction feature

---
+++ client/build/precache-manifest.59d924f6482ab6a2c68d1a1e3d355d81.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dfe35bfdd80a5c9c42e437d5e67c8e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0bb782dac5051e1468c6",
+    "url": "/static/js/2.ffbe907d.chunk.js"
+  },
+  {
+    "revision": "bf249653a7796cf3cd49",
+    "url": "/static/js/main.3f2c2239.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
